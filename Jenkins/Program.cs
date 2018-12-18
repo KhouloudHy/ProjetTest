3373cc9bcd844c8dc185c6be16762c9f2da8897b
Khouloud <hadjyoussef.khouloud@gmail.com>
2018-12-18 11:22:50 +0100

    C1

---
+++ Jenkins/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+            //Console.ReadKey();
+        }
+
+        public static String createMessage()
+        {
+            return "Hello World";
+        }
+
+    }
+}
